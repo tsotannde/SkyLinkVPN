a52a45794c25a21340f6bd9336bce86fd97e2f82
Adebayo Sotannde <me@adebayosotannde.com>
2025-11-07 02:28:27 -0600

    Saving Current State of Application

---
+++ Complete/NotificationCenter.swift
@@ -0,0 +1,17 @@
+//
+//  NitificationCenter.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 10/22/25.
+//
+
+
+import Foundation
+
+extension Notification.Name
+{
+    static let serverDidUpdate = Notification.Name("serverDidUpdate")
+    static let configurationDidChange = Notification.Name("configurationDidChange")
+    static let internetDidConnect = Notification.Name("internetDidConnect")
+    static let internetDidDisconnect = Notification.Name("internetDidDisconnect")
+}
