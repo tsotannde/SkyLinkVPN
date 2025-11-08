bc489f66a4065442ee34e170ea359a024fe46f53
Adebayo Sotannde <me@adebayosotannde.com>
2025-11-08 03:34:16 -0600

    working oin connecton state

---
+++ Complete/Models/ServerDatabase.swift
@@ -0,0 +1,16 @@
+//
+//  ServerDatabase.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 10/22/25.
+//
+
+struct ServerDatabase: Codable
+{
+    let servers: [String: Country]
+}
+
+
+
+
+
