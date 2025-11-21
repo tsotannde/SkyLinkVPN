c214a27305c6578712363793153f938f96bf084f
Adebayo Sotannde <148028245+tsotannde@users.noreply.github.com>
2025-11-21 15:22:06 -0600

    Created and added app icon via Canva

---
+++ SkyLink/AppLogger.swift
@@ -0,0 +1,8 @@
+//
+//  AppLogger.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 11/21/25.
+//
+
+import Foundation
---
+++ SkyLink/LogsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LogsViewController.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 11/21/25.
+//
+
+import Foundation
